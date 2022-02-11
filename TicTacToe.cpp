430a11b4004facd230e8e15f36eac427ad9b4d25
SpencerFurgerson <spfu2841@gmail.com>
2022-02-10 19:46:26 -0700

    GetPlayerChoice git commit -m GetPlayerChoice

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -43,6 +43,144 @@ void DisplayBoard(Board *Table) {
   cout << endl;
 }
 
+void GetPlayerChoice(int Player, Board *Table) {
+
+  cout << "Where will you move?" << endl;
+
+  cout << " 1|2|3 " << endl;
+  cout << "--|-|-- " << endl;
+  cout << " 4|5|6 " << endl;
+  cout << "--|-|-- " << endl;
+  cout << " 7|8|9 " << endl;
+  cout << endl;
+
+  bool moved = false;
+
+  while (moved == false) {
+
+    int move;
+    cin >> move;
+
+    switch (move) {
+
+      case 1:
+        cout << Table->ray[0][0] << endl;
+        if (Table->ray[0][0] != 0) {
+
+          cout << "That spot is taken!" << endl;
+          cout << endl;
+          continue;
+
+        }
+        PlaceMarker(Table,Player,0,0);
+        return;
+        break;
+
+      case 2:
+        if (Table->ray[0][1] != 0) {
+
+          cout << "That spot is taken!" << endl;
+          cout << endl;
+          continue;
+
+        }
+        PlaceMarker(Table,Player,0,1);
+        return;
+        break;
+
+      case 3:
+        if (Table->ray[0][2] != 0) {
+
+          cout << "That spot is taken!" << endl;
+          cout << endl;
+          continue;
+
+        }
+        PlaceMarker(Table,Player,0,2);
+        return;
+        break;
+
+      case 4:
+        if (Table->ray[1][0] != 0) {
+
+          cout << "That spot is taken!" << endl;
+          cout << endl;
+          continue;
+
+        }
+        PlaceMarker(Table,Player,1,0);
+        return;
+        break;
+
+      case 5:
+        if (Table->ray[1][1] != 0) {
+
+          cout << "That spot is taken!" << endl;
+          cout << endl;
+          continue;
+
+        }
+        PlaceMarker(Table,Player,1,1);
+        return;
+        break;
+
+      case 6:
+        if (Table->ray[1][2] != 0) {
+
+          cout << "That spot is taken!" << endl;
+          cout << endl;
+          continue;
+
+        }
+        PlaceMarker(Table,Player,1,2);
+        return;
+        break;
+
+      case 7:
+        if (Table->ray[2][0] != 0) {
+
+          cout << "That spot is taken!" << endl;
+          cout << endl;
+          continue;
+
+        }
+        PlaceMarker(Table,Player,2,0);
+        return;
+        break;
+
+      case 8:
+        if (Table->ray[2][1] != 0) {
+
+          cout << "That spot is taken!" << endl;
+          cout << endl;
+          continue;
+
+        }
+        PlaceMarker(Table,Player,2,1);
+        return;
+        break;
+
+      case 9:
+        if (Table->ray[2][2] != 0) {
+
+          cout << "That spot is taken!" << endl;
+          cout << endl;
+          continue;
+
+        }
+        PlaceMarker(Table,Player,2,2);
+        return;
+        break;
+    }
+
+    cout << "Invalid, try again!" << endl;
+    cout << endl;
+
+  }
+
+
+}
+
 int main() {
 
   CreateBoard();
